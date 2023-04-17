11e54e992001fd672557ad490acc0cac08fe6df5
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-04-18 00:30:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function insert_Row() {
     //Write your code here
-  
+   let tr = document.createElement("tr");
+	let td = document.createElement("td");
+	let table = document.getElementById("sampleTable");
+	const row = table.insertRow(0);
+	const cell1 = row.insertCell(0);
+	cell1.innerText = "new cell1";
+	const cell2 = row.insertCell(1);
+	cell2.innerText = "new cell2"
+	
+	
   
 }
